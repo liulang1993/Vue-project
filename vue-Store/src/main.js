b47f83ffb9d28976dd569d518ecce10a476ba7aa
liulangt <liulang19930413@gmail.com>
2017-08-08 01:00:07 +0800

    Vue-Store first commit

---
+++ vue-Store/src/main.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import VueResource from 'vue-resource'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
+import OrderListPage from './pages/orderList'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+let router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			component: IndexPage
+		},
+		{
+			path: '/orderList',
+			component: OrderListPage
+		},
+		{
+			path:'/detail',
+			component:DetailPage,
+			redirect:'/detail/count',
+			children:[
+				{
+					path:'analysis',
+					component:DetailAnaPage
+				},
+				{
+					path:'count',
+					component:DetailCouPage
+				},
+				{
+					path:'forecast',
+					component:DetailForPage
+				},
+				{
+					path:'publish',
+					component:DetailPubPage
+				},
+			]
+		}
+	]
+})
+
+/* eslint-disable no-new */
+new Vue ({
+  	el: '#app',
+  	router,
+  	template: '<Layout/>',
+  	components: { Layout }
+})
